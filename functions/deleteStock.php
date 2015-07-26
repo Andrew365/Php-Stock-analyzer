0cfe22b02a0310f838751ef1eabdca1986b6f847
AWboy <baseball_allstar_365@hotmail.com>
2015-07-26 17:19:12 -0600

    just some updates, header on delete

--- functions/deleteStock.php
+++ functions/deleteStock.php
@@ -1,29 +1,35 @@
 <?php
-require '../includes/connect.php';
-$file = "../tickerMaster.txt";
-$ticker = $_POST['ticker'];
- $sql = "DROP TABLE {$ticker}";
- $data = mysqli_query($connect, $sql);
 
- $sql2 = "DELETE FROM `analysis_a` WHERE ticker = '{$ticker}'";
- $data2 = mysqli_query($connect, $sql2);
+function deleteStock(){
+    require '../includes/connect.php';
+    $file = "../tickerMaster.txt";
+    $ticker = $_POST['ticker'];
+     $sql = "DROP TABLE {$ticker}";
+     $data = mysqli_query($connect, $sql);
 
- if($data){
+     $sql2 = "DELETE FROM `analysis_a` WHERE ticker = '{$ticker}'";
+     $data2 = mysqli_query($connect, $sql2);
 
-   echo "Table {$ticker} deleted" . '<br />';
+     if($data){
+
+       header('Location: ../index.php');
+
+     }
+     else{
+    echo  mysqli_error($connect);
+     }
+     if($data2){
+
+     }else{
+       echo  mysqli_error($connect);
+     }
+     $sql4 = "DELETE FROM `tickers` WHERE ticker = '{$ticker}'";
+     $result4 = mysqli_query($connect, $sql4);
+     if($result4){
+
+     }elseif (!$result4) {
+      echo mysqli_error($connect);
+     }
+
  }
- else{
-echo  mysqli_error($connect);
- }
- if($data2){
-   echo "Row {$ticker} deleted from analysis_a";
- }else{
-   echo  mysqli_error($connect);
- }
- $sql4 = "DELETE FROM `tickers` WHERE ticker = '{$ticker}'";
- $result4 = mysqli_query($connect, $sql4);
- if($result4){
-   echo "ticker {$ticker} removed from tickers";
- }elseif (!$result4) {
-  echo mysqli_error($connect);
- }
+ deleteStock();
--- index.php
+++ index.php
@@ -8,9 +8,14 @@ require 'templates/header.php';
     <link rel="stylesheet" href="public/stylesheets/index.css"/>
     <meta charset="utf-8">
     <title></title>
+    <script type="text/javascript">
+
+    </script>
   </head>
   <body>
-    <h1>Dashboard</h1>
+
+
+        <h1>Dashboard</h1>
       <div class="container">
         <h3>Long-Term Stock Analysis</h3>
         <hr />
