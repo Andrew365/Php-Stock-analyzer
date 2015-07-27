fba07ae64bf5f32c06aa3f9ce45d5cecfd56535f
AWboy <baseball_allstar_365@hotmail.com>
2015-07-26 20:42:19 -0600

    something

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,14 @@
 <?php
+
+$env = "dev";
+
+if($dev == "dev"){
 $connect = mysqli_connect('localhost', 'Andrew', 'baseball365', 'stock2');
-
-
 if(!$connect){die('connection failed');}
 if($connect){}else{echo"connection failed" .mysqli_connect_error();}
+}
+elseif ($env =="prod") {
+  $connect = mysqli_connect('localhost', 'Andrew', 'baseball365', 'stock2');
+//change this to prod creditials
+}
 ?>
